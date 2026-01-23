31865cdb8a8fff1271febea447e0f7b3d54f2d24
Nico Deblauwe <nico@bluepundit.eu>
2026-01-23 12:06:25 +0100

    BUGFIX :bug: Fix small issues of the data generation + improve AI context

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
@@ -23,13 +24,18 @@ class Group extends Model
         return $this->hasMany(Group::class, 'parent_id');
     }
 
-    public function articles(): HasMany
+    public function articles(): BelongsToMany
     {
-        return $this->hasMany(Article::class);
+        return $this->belongsToMany(Article::class);
     }
 
-    public function activities(): HasMany
+    public function activities(): BelongsToMany
     {
-        return $this->hasMany(Activity::class);
+        return $this->belongsToMany(Activity::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
     }
 }
