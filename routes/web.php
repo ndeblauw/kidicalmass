c4b4264ff55bbd389a50ef8af68ec39bff374dd9
Nico Deblauwe <nico@bluepundit.eu>
2026-01-23 10:22:47 +0100

    REFACTOR :hammer: Switch to dedicated welcome controller

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', \App\Http\Controllers\WelcomeController::class)->name('home');
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])  
