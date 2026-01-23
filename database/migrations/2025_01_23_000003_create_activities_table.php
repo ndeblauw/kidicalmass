1ea247a068bf8fef5ad4704764667d776d463750
Nico Deblauwe <nico@bluepundit.eu>
2026-01-23 22:08:37 +0100

    improve

--- database/migrations/2025_01_23_000003_create_activities_table.php
+++ database/migrations/2025_01_23_000003_create_activities_table.php
@@ -11,15 +11,17 @@ return new class extends Migration
     {
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
+
             $table->string('title_nl');
             $table->string('title_fr');
             $table->text('content_nl');
             $table->text('content_fr');
-            $table->string('activity_type')->default(ActivityType::KIDICALMASS->value);
+            $table->enum('activity_type', array_keys(ActivityType::getOptionsArray()))->default(ActivityType::KIDICALMASS->value);
             $table->dateTime('begin_date');
             $table->dateTime('end_date');
             $table->string('location');
             $table->foreignId('author_id')->constrained('users')->onDelete('cascade');
+
             $table->timestamps();
         });
 
