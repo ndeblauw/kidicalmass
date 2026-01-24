69c38859bd204652d40bc69a0ce60b0df99a9707
Nico Deblauwe <nico@bluepundit.eu>
2026-01-24 15:17:36 +0100

    Seeding works

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,12 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        $this->createGroupHierarchy();
+        // Seed groups first
+        $groupSeeder = new GroupSeeder;
+        $groupSeeder->run();
+        $this->allGroups = $groupSeeder->getAllGroups();
+        $this->mainGroups = $groupSeeder->getMainGroups();
+
         $this->seedUsers();
         $this->seedArticles();
         $this->seedActivities();
@@ -29,77 +34,6 @@ class DatabaseSeeder extends Seeder
         // Clean up temporary images
         \Database\Factories\ArticleFactory::cleanupTempImages();
         \Database\Factories\ActivityFactory::cleanupTempImages();
-    }
-
-    private function createGroupHierarchy(): void
-    {
-        $belgium = Group::create([
-            'shortname' => 'belgium',
-            'name' => 'Belgium',
-            'zip' => null,
-        ]);
-
-        $regions = [
-            ['shortname' => 'flanders', 'name' => 'Flanders', 'zip' => null],
-            ['shortname' => 'brussels-capital-region', 'name' => 'Brussels Capital Region', 'zip' => null],
-            ['shortname' => 'wallonia', 'name' => 'Wallonia', 'zip' => null],
-        ];
-
-        $regionGroups = [];
-        foreach ($regions as $region) {
-            $regionGroups[$region['shortname']] = Group::create([
-                'shortname' => $region['shortname'],
-                'name' => $region['name'],
-                'zip' => $region['zip'],
-                'parent_id' => $belgium->id,
-            ]);
-        }
-
-        $subgroupData = [
-            'flanders' => [
-                ['shortname' => 'antwerp', 'name' => 'Antwerp', 'zip' => '2000'],
-                ['shortname' => 'gent', 'name' => 'Gent', 'zip' => '9000'],
-                ['shortname' => 'leuven', 'name' => 'Leuven', 'zip' => '3000'],
-                ['shortname' => 'hasselt', 'name' => 'Hasselt', 'zip' => '3500'],
-                ['shortname' => 'brugge', 'name' => 'Brugge', 'zip' => '8000'],
-            ],
-            'brussels-capital-region' => [
-                ['shortname' => 'brussels', 'name' => 'Brussels', 'zip' => '1000'],
-                ['shortname' => 'ixelles', 'name' => 'Ixelles', 'zip' => '1050'],
-                ['shortname' => 'anderlecht', 'name' => 'Anderlecht', 'zip' => '1070'],
-                ['shortname' => 'schaerbeek', 'name' => 'Schaerbeek', 'zip' => '1030'],
-                ['shortname' => 'sint-joost-ten-node', 'name' => 'Sint-Joost-ten-Node', 'zip' => '1210'],
-                ['shortname' => 'molenbeek', 'name' => 'Molenbeek', 'zip' => '1080'],
-            ],
-            'wallonia' => [
-                ['shortname' => 'mons', 'name' => 'Mons', 'zip' => '7000'],
-                ['shortname' => 'liege', 'name' => 'Liège', 'zip' => '4000'],
-                ['shortname' => 'arlon', 'name' => 'Arlon', 'zip' => '6700'],
-                ['shortname' => 'namur', 'name' => 'Namur', 'zip' => '5000'],
-                ['shortname' => 'nivelles', 'name' => 'Nivelles', 'zip' => '1300'],
-            ],
-        ];
-
-        $this->allGroups = collect([$belgium]);
-        $this->mainGroups = collect($regionGroups);
-
-        foreach ($regionGroups as $regionShortname => $regionGroup) {
-            $availableSubgroups = $subgroupData[$regionShortname];
-            $subgroupCount = rand(1, 5);
-            $selectedSubgroups = collect($availableSubgroups)->random($subgroupCount);
-
-            foreach ($selectedSubgroups as $subgroup) {
-                $group = Group::create([
-                    'shortname' => $subgroup['shortname'],
-                    'name' => $subgroup['name'],
-                    'zip' => $subgroup['zip'],
-                    'parent_id' => $regionGroup->id,
-                ]);
-                $this->allGroups->push($group);
-            }
-        }
-
-        $this->allGroups = $this->allGroups->merge($this->mainGroups);
     }
 
     private function seedUsers(): void
