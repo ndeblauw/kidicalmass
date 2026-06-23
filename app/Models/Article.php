2844b44c4d3cc9ebf41998c69d59e7320b8fcf27
Shift <shift@laravelshift.com>
2026-06-23 11:41:51 +0000

    Adopt PHP attributes for class properties

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Concerns\HasMainImage;
 use App\Models\Scopes\LocalGroupScope;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
+use Illuminate\Database\Eloquent\Attributes\Unguarded;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -14,14 +15,13 @@ use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
+#[Unguarded]
 #[ScopedBy([LocalGroupScope::class])]
 class Article extends Model implements HasMedia
 {
     use HasFactory;
     use HasMainImage;
     use InteractsWithMedia;
-
-    protected $guarded = [];
 
     public function registerMediaConversions(?Media $media = null): void
     {
--- app/Models/Group.php
+++ app/Models/Group.php
@@ -8,6 +8,7 @@ use App\Models\Concerns\HasMainImage;
 use App\Models\Scopes\LocalGroupScope;
 use Carbon\CarbonInterface;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
+use Illuminate\Database\Eloquent\Attributes\Unguarded;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -19,14 +20,13 @@ use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
+#[Unguarded]
 #[ScopedBy([LocalGroupScope::class])]
 class Group extends Model implements HasMedia
 {
     use HasFactory;
     use HasMainImage;
     use InteractsWithMedia;
-
-    protected $guarded = [];
 
     protected $casts = [
         'started_at' => 'date',
